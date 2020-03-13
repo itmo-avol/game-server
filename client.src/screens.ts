e7c02237e5d9091f5abe88ab130a6ccaa9fa509d
Andrey Volynkin <mail.avol@gmail.com>
2020-03-14 00:31:20 +0300

    Добавляет обработку экранов приложения на клиенте

---
+++ client.src/screens.ts
@@ -0,0 +1,69 @@
+/**
+ * Экраны приложения
+ */
+const screens = getScreens();
+
+/**
+ * Возможные экраны
+ */
+type Screen = keyof typeof screens;
+
+/**
+ * Возвращает карту экранов приложения
+ */
+function getScreens()
+{
+	const waiting: HTMLElement | null = document.querySelector( 'main.waiting' );
+	
+	if ( !waiting )
+	{
+		throw new Error('Can\'t find "waiting" screen');
+	}
+	
+	const game: HTMLElement | null = document.querySelector( 'main.game' );
+	
+	if ( !game )
+	{
+		throw new Error('Can\'t find "waiting" screen');
+	}
+	
+	return {
+		waiting,
+		game,
+	};
+}
+
+/**
+ * Открывает указанный экран
+ * 
+ * @param screen Название экрана, на который переключиться
+ */
+function openScreen( screen: Screen ): void
+{
+	for ( const [key, value] of Object.entries( screens ) )
+	{
+		value.hidden = ( key !== screen );
+	}
+}
+
+/**
+ * Возвращает элемент текущего экрана
+ */
+function getCurrentScreen(): (typeof screens)[keyof (typeof screens)]
+{
+	for ( const screen of Object.values( screens ) )
+	{
+		if ( !screen.hidden )
+		{
+			return screen;
+		}
+	}
+	
+	throw new Error('Can\'t find current screen');
+}
+
+export {
+	screens,
+	openScreen,
+	getCurrentScreen,
+};
